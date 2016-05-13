d7528659c322055f72175b5e8000c1127958cbcc
Lancal <ivda.calderon@gmail.com>
2016-05-13 17:45:16 -0300

    second commit

--- src/taller1automatasversionfinal/Automata.java
+++ src/taller1automatasversionfinal/Automata.java
@@ -1,3 +1,5 @@
+package taller1automatasversionfinal;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
