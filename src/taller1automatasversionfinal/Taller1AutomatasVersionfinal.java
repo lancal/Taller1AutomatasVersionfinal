e04aa7cc288f3f588312701d629ddff52e683a7b
Lancal <ivda.calderon@gmail.com>
2016-05-13 17:42:51 -0300

    first commit

---
+++ src/taller1automatasversionfinal/Taller1AutomatasVersionfinal.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller1automatasversionfinal;
+
+import java.io.*;
+
+
+/**
+ *
+ * @author alanpierrecalderonsantibanez
+ */
+public class Taller1AutomatasVersionfinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ingrese Nombre del Archivo NFA a Convertir: ");
+        String nombre = lectura.readLine();
+        Automata automata = new Automata();
+        automata.LecturaNFA(nombre);
+        
+        
+    }
+    
+}
